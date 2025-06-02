e8005d0aac4368d1a68d40f0a35f72ed2961bde1
tanvirmollik007 <tanvirofficial007@gmail.com>
2025-06-02 21:16:21 +0600

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to BETBD71</h1>
+      <p>This is the home page of your casino site.</p>
+    </div>
+  );
+};
+
+export default Home;
