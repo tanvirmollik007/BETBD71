b65dd3c448f104eeaad053a131ac8f126d21ed04
tanvirmollik007 <tanvirofficial007@gmail.com>
2025-06-02 22:58:48 +0600

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,14 +1,24 @@
+// src/App.jsx
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import PrivateRoute from './components/PrivateRoute'; // নতুনভাবে Import করতে হবে
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              <Home />
+            </PrivateRoute>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
